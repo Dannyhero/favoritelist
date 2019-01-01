b9f17ad2332c02a3d7f21813355e6d1bac271eb8
dannyhero <dannyhero3426@gmail.com>
2019-01-01 15:16:26 +0000

    README.md #7 have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -1,6 +1,6 @@
 class OwnershipsController < ApplicationController
   def create
-    @item = Item.find_or_initialize_by(code: params[:item_code])
+   @item = Item.find_or_initialize_by(code: params[:item_code])
 
     unless @item.persisted?
       # @item が保存されていない場合、先に @item を保存する
@@ -16,15 +16,26 @@ class OwnershipsController < ApplicationController
       flash[:success] = '商品を Want しました。'
     end
 
+    # Have 関係として保存
+    if params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました。'
+    end
+
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    @item = Item.find(params[:item_id])
+   @item = Item.find(params[:item_id])
 
     if params[:type] == 'Want'
       current_user.unwant(@item) 
       flash[:success] = '商品の Want を解除しました。'
+    end
+
+    if params[:type] == 'Have'
+      current_user.unhave(@item) 
+      flash[:success] = '商品の Have を解除しました。'
     end
 
     redirect_back(fallback_location: root_path)
